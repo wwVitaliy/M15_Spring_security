2614c3dc5119215e8a12dbda946c58ec673ce76d
Vitalii Shliakhetko <wwvitaliy@gmail.com>
2024-01-01 19:00:28 +0200

    Task #1 done

---
+++ src/main/java/com/example/TodoList/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.TodoList.controller;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("--->>> TestController created.");
+    }
+
+    @GetMapping(value = "/test")
+    public ModelAndView testMethod() {
+        ModelAndView result = new ModelAndView("test");
+        return result;
+    }
+
+}
